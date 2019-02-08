63198b69db6093cafe7da5eab3ef7fc2163f93dd
hokarimasahiro <42521359+hokarimasahiro@users.noreply.github.com>
2019-02-09 06:28:16 +0900

    create

--- main.ts
+++ main.ts
@@ -1,7 +1,50 @@
-basic.showLeds(`
-    . . . . .
-    . # . # .
-    . . . . .
-    # . . . #
-    . # # # .
-    `);
+/**
+ * makecode CSV(comma separated value) Package.
+ */
+
+/**
+ * CSV block
+ */
+//% weight=100 color=#303030 icon="\u2712" block="CSV"
+namespace CSV {
+
+    /**
+     * get string in a csv data
+     * @param csv data, eg: "123,4,324"
+     * @param n of , eg: 1
+     */
+    //% blockId="GetString" block="Get String in %csv at %n"
+    //% weight=100 blockGap=8
+    export function getString(csv: string, n: number): string {
+        let i1=0
+        let i2=csv.indexOf(",")
+        for(let i=0;i<n;i++,i1=i2){
+            i2=csv.indexOf(",",i1)
+            if ((i2==-1) && i <(n-1)) return ""
+        }
+        if(i2==-1) i2=csv.length
+        return csv.substr(i1,i2-i1)
+    }
+    /**
+     * string to value
+     * @param s data, eg: "123"
+     */
+    //% blockId="toNumber" block="string %s to value"
+    //% weight=98 blockGap=8
+    export function toNumber(s: string): number {
+        let a = 0
+        let b = a.toString()
+        return 0
+    }
+    /**
+     * get value in a csv data
+     * @param csv data, eg: "123,4,324"
+     * @param n of , eg: 1
+     */
+    //% blockId="GetValue" block="Get value in %csv at %n"
+    //% weight=96 blockGap=8
+    export function getValue(csv: string, n: number): number {
+        return toNumber(getString(csv,n))
+    }
+
+}
