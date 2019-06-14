e2bbcdd1585a5c3c1b308f666009d0780833829b
hokarimasahiro <42521359+hokarimasahiro@users.noreply.github.com>
2019-06-15 05:48:54 +0900

    test

--- test.ts
+++ test.ts
@@ -1,5 +1,5 @@
 // tests go here; this will not be compiled when this package is used as a library
 basic.forever(function () {
-    basic.showNumber(CSV.getValue("123,4,324",4))
+    basic.showNumber(CSV.getValue("M,123,4,324", 0))
     basic.pause(1000)
 })
