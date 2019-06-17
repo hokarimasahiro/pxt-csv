733854225194f87ff352ecf36af9a5841cd433a6
hokarimasahiro <42521359+hokarimasahiro@users.noreply.github.com>
2019-06-17 18:28:07 +0900

    update

--- kana.cpp
+++ kana.cpp
@@ -1,1 +1,8 @@
- #include "pxt.h"
+#include "pxt.h"
+
+namespace csv{
+    uint32_t testI2cRead(int32_t ad){
+        Buffer buf = createBuffer(2);
+        return uBit.i2c.read(ad << 1 | 1,buf,2)
+    } 
+}
