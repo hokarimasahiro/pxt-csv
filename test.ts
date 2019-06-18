438e7226c11c188b9aecbab00a67b5c04dec9a42
hokarimasahiro <42521359+hokarimasahiro@users.noreply.github.com>
2019-06-19 05:57:04 +0900

    update

--- test.ts
+++ test.ts
@@ -1,17 +1,6 @@
 // tests go here; this will not be compiled when this package is used as a library
-let testStr = "M,123,4325,11,5"
-let testCount = 6
+
 basic.forever(function () {
-    for (let i = 0; i < testCount; i++) {
-        basic.clearScreen()
-        basic.showString(CSV.getString(testStr, i))
-        basic.pause(500)
-    }
-    basic.pause(500)
-    for (let i = 0; i < testCount; i++) {
-        basic.clearScreen()
-        basic.showNumber(CSV.getValue(testStr, i))
-        basic.pause(500)
-    }
+    basic.showNumber(CSV.testI2cRead(0x32))
     basic.pause(500)
 })
