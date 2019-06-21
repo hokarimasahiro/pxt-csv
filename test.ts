0fec8a44f0583f773741743248952ffece307f9d
hokarimasahiro <42521359+hokarimasahiro@users.noreply.github.com>
2019-06-22 05:27:41 +0900

    update

--- test.ts
+++ test.ts
@@ -1,4 +1,8 @@
 // tests go here; this will not be compiled when this package is used as a library
 
 basic.forever(function () {
+    basic.clearScreen()
+    basic.pause(100)
+    basic.showString(CSV.getString("ABC,DEF,123",2))
+    basic.pause(300)
 })
