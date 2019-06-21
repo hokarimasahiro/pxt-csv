56b93ebc179f88b6e21bbe55690d6a7ec8b34c84
hokarimasahiro <42521359+hokarimasahiro@users.noreply.github.com>
2019-06-22 05:19:03 +0900

    del

--- kana.cpp
+++
@@ -1,9 +0,0 @@
-#include "pxt.h"
-
-namespace CSV{
-    uint32_t testI2c(int32_t ad){
-        Buffer buf = createBuffer(2);
-        return uBit.i2c.read(ad << 1 | 1,buf,2)
-        return 2
-    } 
-}
